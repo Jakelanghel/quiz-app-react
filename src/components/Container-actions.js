6df4db11daeb0923dc715191fc2606fba5445412
jake langhel <jacobtlanghel@gmail.com>
2022-03-07 20:42:43 -0500

    refactor conatiner-actions styles

--- src/components/Container-actions.js
+++ src/components/Container-actions.js
@@ -3,31 +3,35 @@ import "./Container-actions.css";
 
 const ContainerActions = (props) => {
     return (
-        <div className='container-actions'>
-            {props.quizData.errors && (
-                <p className='msg error-msg'>
-                    please answer ALL questions to continue ! ! !
-                </p>
-            )}
-            {props.quizData.quizComplete && (
-                <p className='msg score-msg'>
-                    {`you scored ${props.quizData.score} / ${props.quizData.questionsArr.length} correct answers.`}
-                </p>
-            )}
+        <div className='actions-background'>
+            <div className='container-actions'>
+                {props.quizData.errors && (
+                    <p className='msg error-msg'>
+                        please answer ALL questions to continue
+                    </p>
+                )}
+                {props.quizData.quizComplete && (
+                    <p className='msg score-msg'>
+                        {`you scored ${props.quizData.score} / ${props.quizData.questionsArr.length} correct answers.`}
+                    </p>
+                )}
 
-            <button
-                className='check-btn btn'
-                onClick={
-                    props.quizData.quizComplete
-                        ? props.resetGame
-                        : props.gradeQuiz
-                }
-            >
-                {props.quizData.quizComplete ? "Play Again" : "Check Answers"}
-            </button>
-            <button className='main-menu-btn' onClick={props.backToMenu}>
-                main menu
-            </button>
+                <button
+                    className='check-btn btn'
+                    onClick={
+                        props.quizData.quizComplete
+                            ? props.resetGame
+                            : props.gradeQuiz
+                    }
+                >
+                    {props.quizData.quizComplete
+                        ? "Play Again"
+                        : "Check Answers"}
+                </button>
+                <button className='main-menu-btn' onClick={props.backToMenu}>
+                    main menu
+                </button>
+            </div>
         </div>
     );
 };
